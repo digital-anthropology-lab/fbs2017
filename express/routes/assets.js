40334d2f11fb16635e327465a32f365f8eb42f39
garethfoote <gareth.foote@gmail.com>
2017-10-02 10:01:00 +0100

    Create asset page

--- express/routes/assets.js
+++ express/routes/assets.js
@@ -14,6 +14,42 @@ module.exports = function(io) {
 
   });
 
+  router.get('/create', function(req, res, next) {
+    res.render('asset-create', { title: "Create Garment Asset" });
+  })
+
+  router.post('/create', function(req, res, next) {
+
+    // mc.listAssetsPromise()
+    //   .then(function(data){
+    //     // if()
+    //     // res.render('assets', { title: "Assets", assets : data });
+    //   })
+
+    mc.getLocalChainUsers().then(function(data){
+      var address = data.filter(function(item){ return item.ismine })
+      return mc.issuePromise({
+        address: address[0].address,
+        asset: {
+          name: req.body.name,
+          open: true
+        },
+        qty: parseInt(req.body.assetvalue),
+        units: 1,
+        details: {
+          "description": req.body.description
+        }
+      })
+    }).then(function(data){
+      console.log(data)
+      res.render('assets', { title: "Issue Asset success", assets : data });
+    }).error(function(e){
+      console.log(e)
+      res.send(e)
+    })
+
+  })
+
   router.get('/:name', function(req, res, next) {
 
     mc.listAssets({asset: req.params.name, verbose: true}, function(err, data){
@@ -22,30 +58,6 @@ module.exports = function(io) {
 
   });
 
-  router.get('/create/:name/:qty/:address', function(req, res, next) {
-
-    // mc.listAssetsPromise()
-    //   .then(function(data){
-    //     // if()
-    //     // res.render('assets', { title: "Assets", assets : data });
-    //   })
-
-    mc.issuePromise({
-      address: req.params.address,
-      asset: {
-        name: req.params.name,
-        open: true
-      },
-      qty: parseInt(req.params.qty),
-      units: 0.1
-    }).then(function(data){
-      console.log(data)
-      res.render('assets', { title: "Issue Asset", assets : data });
-    }).error(function(e){
-      console.log(e)
-    });
-
-  });
 
   return router;
 }
